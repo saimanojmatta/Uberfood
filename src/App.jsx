25ddefadfda02afe8475dceee6b4d58149ce5098
saimanojmatta <142380042+saimanojmatta@users.noreply.github.com>
2023-12-11 07:32:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Category from "./components/Category"
+import Food from "./components/Food"
+import Headlinecards from "./components/Headlinecards"
+import Hero from "./components/Hero"
+
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <div>
+    <Navbar/>
+    <Hero/>
+    <Headlinecards/>
+    <Food/>
+    <Category/>
+   
+    </div>
+  )
+}
+export default App
+ 
